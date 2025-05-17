bbdaa14a1a1a82650704382375005bec29f69e92
maurolsmoura <maurolsmoura@gmail.com>
2025-05-17 17:18:27 +0100

    adjusted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./",
+  base: "/", // ✅ correct for modernizese.com
   build: {
     outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          console.log("Chunk path:", id);
-          return null;
-        },
-      },
-    },
   },
-}));
+});
